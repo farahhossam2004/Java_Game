63aed29d52aa70f930dd8532eb156991781f2db5
Farah Hossam <farah.alden2004@gmail.com>
2024-06-14 00:25:48 +0300

    ohh lala

---
+++ NewGameSceneController.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+public class NewGameSceneController {
+    
+    private Stage stage ;
+    private Scene scene;
+    private Parent root;
+
+     // video background
+    @FXML
+    private MediaView mediaview;
+
+    public void initialize() {
+
+        try {
+            String videoPath = "./Images/withouttext.mp4";
+
+            Media media = new Media(new File(videoPath).toURI().toString());
+
+            MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+            
+            mediaview.setMediaPlayer(mediaPlayer);
+            mediaPlayer.play();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //===========================================================================
+
+    // Go To Level Scene
+    public void SwitchtoLevelScene(ActionEvent e)throws IOException{
+        root = FXMLLoader.load(getClass().getResource("LevelScene.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+//=================================================
+
+    //Back to home Scene Button 
+    public void SwitchToHomeScene(ActionEvent e)throws IOException{
+        root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //============================================================
+
+
+}
