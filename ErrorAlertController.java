e9528d38f83c52de693de48d349802c52fa5e4f1
Farah Hossam <farah.alden2004@gmail.com>
2024-06-27 23:50:40 +0300

    sorry mate

--- ErrorAlertController.java
+++ ErrorAlertController.java
@@ -1,21 +1,13 @@
-import java.util.function.Consumer;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
 public class ErrorAlertController {
     
     private Stage stage;
-    private Consumer<ActionEvent> closeAlertFunction;
 
     public void setStage(Stage stagee)
     {
         this.stage = stagee;
-    }
-
-    public void setcloseAlert(Consumer<ActionEvent> event){
-        this.closeAlertFunction = event;
     }
 
     @FXML
