659bcaf47e265b4c26ce1af817f02b89fcf572e5
Farah Hossam <farah.alden2004@gmail.com>
2024-07-02 01:29:06 +0300

    edit credits scene

--- CreditsController.java
+++ CreditsController.java
@@ -1,12 +1,26 @@
 
+import java.io.IOException;
+
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class CreditsController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
     @FXML
     private VBox creditsContainer;
 
@@ -40,5 +54,14 @@ public class CreditsController {
         scaleTransition.setAutoReverse(true);
 
         scaleTransition.playFromStart();
+    }
+
+    //Back to home Scene Button 
+    public void SwitchToHomeScene(ActionEvent e)throws IOException{
+        root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
